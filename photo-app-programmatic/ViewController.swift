891a16fb565a263cb2788ba4b232ecc61d06065d
Jorge Ferrusca <jorge.ferrusca@sailpoint.com>
2024-05-03 23:48:29 -0600

    Home view controller working with programmatic UIView

---
+++ photo-app-programmatic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  photo-app-programmatic
+//
+//  Created by Jorge Ferrusca_C on 03/05/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
